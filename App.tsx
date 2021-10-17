e6a4e1a46277aee8ff008f8983fb6b8f2bfe7e19
Miguel Veloso <obeloke@hotmail.com>
2021-10-17 16:20:21 -0300

    Contador

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native';
+import { ContadorScreen } from './src/screens/ContadorScreen';
 import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 
 const App = () => {
   return (
-    <HolaMundoScreen />
+    // <HolaMundoScreen />
+    <ContadorScreen />
   )
 }
 

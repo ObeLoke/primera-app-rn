1deb6f54875eadc03eeb2614e30fa38b7e25bcfc
Miguel Veloso <obeloke@hotmail.com>
2021-10-17 22:25:57 -0300

    Dimensiones, alto ancho

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { SafeAreaView, Text, View } from 'react-native';
 import { ContadorScreen } from './src/screens/ContadorScreen';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+import { DimensionesScreen } from './src/screens/DimensionesScreen';
 
 const App = () => {
   return (
@@ -10,7 +11,8 @@ const App = () => {
     // <ContadorScreen />
 
     <SafeAreaView style={{ flex: 1 }}>
-      <BoxObjectModelScreen />
+      {/* <BoxObjectModelScreen /> */}
+      <DimensionesScreen />
     </SafeAreaView>
   )
 }

64a1333632530c8660726d0ed834e3a09dd48c04
Miguel Veloso <obeloke@hotmail.com>
2021-10-22 23:48:56 -0300

    Tarea 5

--- src/screens/TareaScreen.tsx
+++ src/screens/TareaScreen.tsx
@@ -25,31 +25,29 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#28425B',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        flexDirection: 'row'
 
     },
     cajaMorada: {
         width: 100,
-        height: 100,
+        // height: 100,
         borderWidth: 10,
         borderColor: 'white',
         backgroundColor: '#5856D6',
-        alignSelf: 'flex-end'
     },
     cajaNaranja: {
         width: 100,
-        height: 100,
+        // height: 100,
         borderWidth: 10,
         borderColor: 'white',
         backgroundColor: '#F0A23B',
-        alignSelf: 'center'
     },
     cajaCeleste: {
         width: 100,
-        height: 100,
+        // height: 100,
         borderWidth: 10,
         borderColor: 'white',
         backgroundColor: '#28C4D9',
-        alignSelf: 'flex-start'
     }
 });
